6f790d65844a202fba3887b0e168f2a7ea92e411
Ram Yewale <ramn.yewale@gmail.com>
2024-08-22 23:22:25 +0530

    feat: initial commit with Spring security basics

---
+++ src/main/java/com/spring/security/model/Student.java
@@ -0,0 +1,14 @@
+package com.spring.security.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+}
